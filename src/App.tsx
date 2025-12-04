23a128910eaf2c7c4cf71c941550c40cd701acaf
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-04 10:45:13 -0500

    Adds Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ExploreTopBooks } from './components/ExploreTopBooks';
 import { Navbar } from './components/Navbar/Navbar';
 import { Heroes } from './components/Heroes';
 import { LibraryServices } from './components/LibraryServices';
+import { Footer } from './components/Footer';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Carousel />
             <Heroes />
             <LibraryServices />
+            <Footer />
         </>
     );
 }
