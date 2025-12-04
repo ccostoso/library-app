fb2862596564c8aac4dfaca734363b20cea0bd6b
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-04 18:27:13 -0500

    Adds repository/api endpoints

--- backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
+++ backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Book.java
@@ -30,6 +30,6 @@ public class Book {
     @Column(name = "category")
     private String category;
 
-    @Column(name = "image")
-    private String image;
+    @Column(name = "img")
+    private String img;
 }
